553be544a2718e883b0f613b99c98975264bc93c
YangC <63809495+AntiCare@users.noreply.github.com>
2025-03-19 05:42:13 +0100

    - add simulator

---
+++ Context-III-Unity/Assets/Plugins/HurricaneVR-Extensions/Hexabody/HVRHexabodySimulator.cs
@@ -0,0 +1,61 @@
+using HexabodyVR.PlayerController;
+using UnityEngine;
+
+namespace HurricaneVRExtensions.Simulator
+{
+    public class HVRHexabodySimulator : HVRBodySimulator
+    {
+        //Hexabody dependencies
+     
+        private HexaBodyPlayer4 _hexabodyPlayer4;
+
+        protected override void Start()
+        {
+            if (!ResolveDependencies())
+            {
+                Debug.Log("Auto resolve dependencies failed: The Rig parameter must be assigned with the Hurricane/Hexabody rig root object.", gameObject);
+                enabled = false;
+                return;
+            }
+            
+        }
+
+        protected override void TurnCamera()
+        {
+            float rotationAngleY = MouseDelta.y * turnSpeed;
+            _hexabodyPlayer4.Camera.transform.RotateAround(_hexabodyPlayer4.Camera.transform.position, _hexabodyPlayer4.Camera.transform.right, -rotationAngleY);
+        }
+
+        protected override void TurnRig()
+        {
+            float rotationAngleX = MouseDelta.x * turnSpeed;
+            _hexabodyPlayer4.Pelvis.transform.RotateAround(_hexabodyPlayer4.Pelvis.transform.position, Vector3.up, rotationAngleX);
+        }
+
+        #region Initialization
+        protected override bool ResolveDependencies()
+        {
+            if (!autoResolveDependencies)
+                return true;
+
+            _hexabodyPlayer4 = Rig.GetComponentInChildren<HexaBodyPlayer4>();
+
+            if (_hexabodyPlayer4 == null)
+            {
+                Debug.Log(DependencyError("HexaBodyPlayer4"));
+                return false;
+            }
+
+
+            return true;
+        }
+
+        private string DependencyError(string component)
+        {
+            return string.Format("HVRHexabodySimulator AutoResolveDependencies Error: No {0} component found. Make sure Rig parameter is assigned with Hurricane/Hexabody rig root object.", component);
+        }
+        #endregion
+
+    }
+}
+
