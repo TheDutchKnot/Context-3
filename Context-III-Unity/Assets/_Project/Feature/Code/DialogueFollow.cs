7a0a8b627319e23b1f9855b19952a45fcab61c53
GinaXkiip <ginadbra@gmail.com>
2025-03-19 14:06:54 +0100

    dialogue retry
    it works except it moves forward when the player looks up or down, need see if i can fix that or we can just have it that it always follows players no matter where they look

--- Context-III-Unity/Assets/_Project/Feature/Code/DialogueFollow.cs
+++ Context-III-Unity/Assets/_Project/Feature/Code/DialogueFollow.cs
@@ -1,32 +1,50 @@
+using System.Collections;
 using UnityEngine;
 
 public class DialogueFollow : MonoBehaviour
 {
-    [SerializeField] Transform cameraTransform;
-    [SerializeField] Vector3 offsetFromCamera;
-    [SerializeField] float turnBufferInDegrees = 30f;
-    [SerializeField] float turnSpeed = 0.03f;
-    [SerializeField] bool lockPitch = false;
+    [SerializeField] private Transform playerHead;
+    [SerializeField] private float distanceFromPlayer = 1.0f;
+    [SerializeField] private float time = 0.3f;
+    [SerializeField] private float yPos = 1.2f;
 
+    [SerializeField] public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    public void OnEnable()
+    {
+       // StartCoroutine(Timer());
+    }
+
+    public void FollowPlayer()
+    {
+        //moves the text to where the player looks
+        Vector3 targetPosition = playerHead.position + playerHead.forward * distanceFromPlayer;
+
+        //makes it so camera doesnt move up or down
+        targetPosition.y = yPos;
+
+        //making it smoooooth
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+    
+
+        //rotates the text to face the player
+        Vector3 directionToCamera = playerHead.position - transform.position;
+        
+        //makes it so camera doesnt rotate up or down
+       directionToCamera.y = 0; 
+        
+        transform.rotation = Quaternion.LookRotation(-directionToCamera, Vector3.up);
+    }
     private void Update()
     {
-        transform.localPosition = cameraTransform.localPosition + offsetFromCamera;
-        Vector3 cameraEuler = cameraTransform.rotation.eulerAngles;
+            FollowPlayer();
+    }
 
-        cameraEuler.z = 0f;
-
-        if(lockPitch)
-        {
-            cameraEuler.x = 0f;
-        }
-
-        Quaternion targetQuat = Quaternion.Euler(cameraEuler);
-
-        float angle = Quaternion.Angle(transform.rotation, targetQuat);
-
-        if(angle > turnBufferInDegrees)
-        {
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetQuat, turnSpeed);
-        }
+    //timer to make the object turn off, can maybe use later to sync with audioclip/voice acting
+    private IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(time);
     }
 }
