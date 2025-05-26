b405a068eaab9fd03522409ed339acbeaaf5f5dd
GinaXkiip <ginadbra@gmail.com>
2025-05-26 13:32:52 +0200

    dialogue box
    lines can be displayed and it goes and ends automatically

---
+++ Context-III-Unity/Assets/_Project/DialogueSystem.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textComponent;
+    [SerializeField] string[] textLines;
+    [SerializeField] float textSpeed;
+    [SerializeField] float TimeUntillnextLine;
+
+    private int index;
+
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        startDialogue();
+    }
+
+    void startDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in textLines[index].ToCharArray() )
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        yield return new WaitForSeconds(TimeUntillnextLine);
+        NextLine();
+    }
+
+    void NextLine()
+    {
+        if (index < textLines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
