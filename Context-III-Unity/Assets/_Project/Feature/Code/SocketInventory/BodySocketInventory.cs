0a1c04480d7b4d138463c3abbcb86589414ddec6
YangC <63809495+AntiCare@users.noreply.github.com>
2025-06-07 04:57:51 +0200

    - [add] split boss mesh

--- Context-III-Unity/Assets/_Project/Feature/Code/SocketInventory/BodySocketInventory.cs
+++ Context-III-Unity/Assets/_Project/Feature/Code/SocketInventory/BodySocketInventory.cs
@@ -10,39 +10,6 @@ public class bodySocket
     public float heightRatio;
 }
 
-// public class BodySocketInventory : MonoBehaviour
-//
-// {
-//     public GameObject HMD;
-//     public bodySocket[] bodySockets;
-//
-//     private Vector3 _currentHMDlocalPosition;
-//     private Quaternion _currentHMDRotation;
-//     void Update()
-//     {
-//         _currentHMDlocalPosition = HMD.transform.localPosition;
-//         _currentHMDRotation = HMD.transform.rotation;
-//         // foreach(var bodySocket in bodySockets)
-//         // {
-//         //     UpdateBodySocketHeight(bodySocket);
-//         // }
-//         UpdateSocketInventory();
-//     }
-//
-//     private void UpdateBodySocketHeight(bodySocket bodySocket)
-//     {
-//
-//         bodySocket.gameObject.transform.localPosition = new Vector3(bodySocket.gameObject.transform.localPosition.x,(_currentHMDlocalPosition.y * bodySocket.heightRatio), bodySocket.gameObject.transform.localPosition.z);
-//     }
-//
-//     private void UpdateSocketInventory()
-//     {
-//         transform.localPosition = new Vector3(_currentHMDlocalPosition.x, 0, _currentHMDlocalPosition.z);
-//         transform.rotation = new Quaternion(transform.rotation.x, _currentHMDRotation.y, transform.rotation.z, _currentHMDRotation.w);
-//     }
-// }
-
-
 public class BodySocketInventory : MonoBehaviour
 
 {
@@ -55,10 +22,10 @@ public class BodySocketInventory : MonoBehaviour
     {
         _currentHMDlocalPosition = HMD.transform.localPosition;
         _currentHMDRotation = HMD.transform.rotation;
-        foreach(var bodySocket in bodySockets)
-        {
-            UpdateBodySocketHeight(bodySocket);
-        }
+        // foreach(var bodySocket in bodySockets)
+        // {
+        //     UpdateBodySocketHeight(bodySocket);
+        // }
         UpdateSocketInventory();
     }
 
@@ -74,3 +41,36 @@ public class BodySocketInventory : MonoBehaviour
         transform.rotation = new Quaternion(transform.rotation.x, _currentHMDRotation.y, transform.rotation.z, _currentHMDRotation.w);
     }
 }
+
+//
+// public class BodySocketInventory : MonoBehaviour
+//
+// {
+//     public GameObject HMD;
+//     public bodySocket[] bodySockets;
+//
+//     private Vector3 _currentHMDlocalPosition;
+//     private Quaternion _currentHMDRotation;
+//     void Update()
+//     {
+//         _currentHMDlocalPosition = HMD.transform.localPosition;
+//         _currentHMDRotation = HMD.transform.rotation;
+//         foreach(var bodySocket in bodySockets)
+//         {
+//             UpdateBodySocketHeight(bodySocket);
+//         }
+//         UpdateSocketInventory();
+//     }
+//
+//     private void UpdateBodySocketHeight(bodySocket bodySocket)
+//     {
+//
+//         bodySocket.gameObject.transform.localPosition = new Vector3(bodySocket.gameObject.transform.localPosition.x,(_currentHMDlocalPosition.y * bodySocket.heightRatio), bodySocket.gameObject.transform.localPosition.z);
+//     }
+//
+//     private void UpdateSocketInventory()
+//     {
+//         transform.localPosition = new Vector3(_currentHMDlocalPosition.x, 0, _currentHMDlocalPosition.z);
+//         transform.rotation = new Quaternion(transform.rotation.x, _currentHMDRotation.y, transform.rotation.z, _currentHMDRotation.w);
+//     }
+// }
